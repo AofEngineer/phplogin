8b48cf6d030bc062b4cdf2e4fc4d96518720b84a
AofEngineer <124555693+AofEngineer@users.noreply.github.com>
2023-09-01 17:07:42 +0700

    commit

--- authenticate.php
+++ authenticate.php
@@ -1,29 +1,25 @@
 <?php
 require("Function.php");
+
 if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
+	// If there is an error with the connection, stop the script and display the error.    
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
-// Now we check if the data from the login form was submitted, isset() will check if the data exists.
 if ( !isset($_POST['username'], $_POST['password']) ) {
 	// Could not get the data that should have been sent.
 	exit('Please fill both the username and password fields!');
 }
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+
+if ($stmt = $con->prepare('SELECT id, password ,activation_code FROM accounts WHERE username = ?')) {
+	
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
-	// Store the result so we can check if the account exists in the database.
+	
 	$stmt->store_result();
     if ($stmt->num_rows > 0) {
-        $stmt->bind_result($id, $password);
+        $stmt->bind_result($id, $password,$activation_code);
         $stmt->fetch();
-        // Account exists, now we verify the password.
-        // Note: remember to use password_hash in your registration file to store the hashed passwords.
-        if (password_verify($_POST['password'], $password)) {
-            // Verification success! User has logged-in!
-            // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+        if (password_verify($_POST['password'], $password)&& $activation_code == "activated") {
             session_regenerate_id();
             $_SESSION['loggedin'] = TRUE;
             $_SESSION['name'] = $_POST['username'];
@@ -31,17 +27,28 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
             echo 'Welcome ' . $_SESSION['name'] . '!';
             header('Location: home.php');
         } else {
-            // Incorrect password
-            echo 'Incorrect username and/or password!';
+            echo '<div class="login">
+                    <p>Incorrect username and/or password!</p>
+                  </div>
+                  <button type="button" onClick="'."location.href='index.html'".'">Logout</button> 
+            ';
         }
     } else {
-        // Incorrect username
-        echo 'Incorrect username and/or password!';
+        echo '<body><div class="login">
+        <p>Incorrect username and/or password!</p>
+      </div>
+      <button type="button" onclick="location.href="index.php";">Logout</button> 
+      </body>';
     }
-    
-
-
 	$stmt->close();
 }
 
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Login</title>		
+        <link rel="stylesheet" href="style.css" type="text/css">
+	</head>
+<html>
--- register.php
+++ register.php
@@ -1,23 +1,10 @@
 <?php
 require("sendmail.php");
 require ("Function.php");
-if (mysqli_connect_errno()) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-// Now we check if the data was submitted, isset() function will check if the data exists.
-if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
-	// Could not get the data that should have been sent.
-	exit('Please complete the registration form!');
-}
-// Make sure the submitted registration values are not empty.
-if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
-	// One or more values are empty.
-	exit('Please complete the registration form');
-}
-// We need to check if the account with that username exists.
+
+
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
+	
 	if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
         exit('Email is not valid!');
     }
@@ -30,34 +17,21 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
     $stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
 	$stmt->store_result();
-	// Store the result so we can check if the account exists in the database.
 	if ($stmt->num_rows > 0) {
-		// Username already exists
 		echo 'Username exists, please choose another!';
 	} else {
-		// Username doesn't exists, insert new account
-        if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email, activation_code) VALUES (?, ?, ?, ?)'))  {
-	// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
+        if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
 	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 	$uniqid = uniqid();
     $stmt->bind_param('ssss', $_POST['username'], $password, $_POST['email'], $uniqid);
 	$stmt->execute();
-	$from    = 'engineer03@guru-itsolution.com';
-$subject = 'Account Activation Required';
-//$headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
-// Update the activation variable below
-$activate_link = 'localhost/phplogin/activate.php?email=' . $_POST['email'] . '&code=' . $uniqid;
-$message = '<p>Please click the following link to activate your account: <a href="' . $activate_link . '">' . $activate_link . '</a></p>';
-mailsend($_POST['email'], $subject, $message ,$from);
-echo 'Please check your email to activate your account!';
+	echo 'You have successfully registered, you can now login!';
 } else {
-	// Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all 3 fields.
 	echo 'Could not prepare statement!';
 }
 	}
 	$stmt->close();
 } else {
-	// Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all 3 fields.
 	echo 'Could not prepare statement!';
 }
 $con->close();
